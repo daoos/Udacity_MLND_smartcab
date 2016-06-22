6d72f19e29d46ce5cf46f89c1132affffd0a5c81
Hang Yao <hang_yao@hotmail.com>
2016-06-22 00:27:51 -0500

    docs: added comments
    Added comments of defined variables for easier reading.
    
    Replaced the variable name 'timer' to 'num_moves' for precise meaning.
    
    Fixed some typos.
    
    No issue tracker ID.

--- agent.py
+++ agent.py
@@ -11,13 +11,13 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
-        self.timer = 0
-        self.num_penalty = 0
-        self.cumulative_rewards = 0
-        self.failure = 0
-        self.penalty_ratio = 0
-        self.num_trial = 0
-        self.success_rate = 0
+        self.num_moves = 0 # Number of Moves
+        self.num_penalty = 0 # Number of Penalty
+        self.cumulative_rewards = 0 # Cumulative Rewards
+        self.failure = 0 # Number of Failure
+        self.penalty_ratio = 0 # Penalty Ratio = Number of Penalty / Number of Moves * 100
+        self.num_trial = 0 # Number of Trials
+        self.success_rate = 0 # Success Rate = 1 - (Number of Failure / Number of Trials)
         self.possible_action = [None, 'forward', 'left', 'right']
         self.Q = {}
         for i in ['green', 'red']: #light
@@ -47,7 +47,7 @@ class LearningAgent(Agent):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
         self.num_trial += 1
-        print "Penalty Ratio: {:.3f}, Success Rate: {:.3f}, Cumulative Rewards: {}, Number of Moves: {}".format(self.penalty_ratio, self.success_rate, self.cumulative_rewards, self.timer)
+        print "Penalty Ratio: {:.3f}, Success Rate: {:.3f}, Cumulative Rewards: {}, Number of Moves: {}".format(self.penalty_ratio, self.success_rate, self.cumulative_rewards, self.num_moves)
 
     def update(self, t):
         # Gather inputs
@@ -56,7 +56,7 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        alpha = 1.0 / (1.2 + float(self.timer) / 60.0)
+        alpha = 1.0 / (1.2 + float(self.num_moves) / 60.0)
         #alpha = 1.0
         gamma = 0.3
         self.state = (inputs['light'],self.next_waypoint,inputs['oncoming'],inputs['left'])
@@ -64,7 +64,7 @@ class LearningAgent(Agent):
         # TODO: Select action according to your policy
         Q_max = self.Q[self.state].index(max(self.Q[self.state]))
         # implement epsilon-greedy approach
-        epsilon = 1.0 / (1.0 + self.timer / 10.0)
+        epsilon = 1.0 / (1.0 + self.num_moves / 10.0)
         if random.random() < epsilon:
             action = random.choice(self.possible_action)
         else:
@@ -78,8 +78,8 @@ class LearningAgent(Agent):
         if deadline == 0:
             self.failure += 1
         self.success_rate = 1.0 - float(self.failure) / float(self.num_trial)
-        self.timer += 1
-        self.penalty_ratio = float(self.num_penalty) / float(self.timer) * 100
+        self.num_moves += 1
+        self.penalty_ratio = float(self.num_penalty) / float(self.number_moves) * 100
 
         # TODO: Learn policy based on state, action, reward
         future_inputs = self.env.sense(self)
@@ -87,7 +87,7 @@ class LearningAgent(Agent):
         future_state = (future_inputs['light'],future_waypoint,future_inputs['oncoming'],future_inputs['left'],)
         self.Q[self.state][self.possible_action.index(action)] = (1 - alpha) * self.Q[self.state][self.possible_action.index(action)] + (alpha * (reward + gamma * max(self.Q[future_state])))
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-        #print "Penalty Ratio: {:.3f}, Success Rate: {:.3f}, Cumulative Rewards: {}, Number of Moves: {}".format(self.penalty_ratio, self.success_rate, self.cumulative_rewards, self.timer)
+        #print "Penalty Ratio: {:.3f}, Success Rate: {:.3f}, Cumulative Rewards: {}, Number of Moves: {}".format(self.penalty_ratio, self.success_rate, self.cumulative_rewards, self.num_moves)
 
 def run():
     """Run the agent for a finite number of trials."""
@@ -100,7 +100,6 @@ def run():
     # Now simulate it
     sim = Simulator(e, update_delay=0.0001)  # reduce update_delay to speed up simulation
     sim.run(n_trials=100)  # press Esc or close pygame window to quit
-
 
 if __name__ == '__main__':
     run()
